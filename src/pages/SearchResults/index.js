388be8841647b2220eed6e657d3a2272b8a756e3
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2021-10-25 12:18:11 -0300

    Css grid y infinite scroll

---
+++ src/pages/SearchResults/index.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useCallback, useRef} from 'react'
+import Spinner from 'components/Spinner'
+import ListOfGifs from 'components/ListOfGifs'
+import {useGifs} from 'hooks/useGifs'
+import useNearScreen from 'hooks/useNearScreen'
+import debounce from 'just-debounce-it'
+
+export default function SearchResults ({ params }) {
+  const { keyword } = params
+  const { loading, gifs, setPage } = useGifs({ keyword })
+  const externalRef = useRef() //visor
+  const {isNearScreen} = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once : false
+  })
+    
+  //const handleNextPage = () => setPage(prevPage => prevPage + 1)
+
+
+  const debounceHandleNextPage = useCallback( debounce (
+    ()=>setPage(prevPage => prevPage + 1), 200
+    ), [])
+
+  useEffect(() => {
+    console.log(isNearScreen)
+
+    if (isNearScreen) debounceHandleNextPage()
+  }, [debounceHandleNextPage, isNearScreen]);
+
+  
+  return <>
+    {loading
+      ? <Spinner />
+      : <>
+        <h3 className="App-title">
+          {decodeURI(keyword)}
+        </h3>
+        <ListOfGifs gifs={gifs} />
+        <div id="visor" ref={externalRef}></div>
+      </>
+    }
+    <br />
+    {/*<button onClick={handleNextPage}>Get next page</button>*/}
+  </>
+}
